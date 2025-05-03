34f7ecbde46090c1bfc2261955be11a38cb69cdc
AFieldman <95adam.akerman@googlemail.com>
2025-05-03 12:43:16 +0200

    fix faulty ns reference

--- plugin/source/PluginProcessor.cpp
+++ plugin/source/PluginProcessor.cpp
@@ -174,5 +174,5 @@ void AudioPluginAudioProcessor::setStateInformation(const void* data, int sizeIn
 // This creates new instances of the plugin.
 // This function definition must be in the global namespace.
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
-  return new audio_plugin::AudioPluginAudioProcessor();
+  return new waveshaper_plugin::AudioPluginAudioProcessor();
 }
